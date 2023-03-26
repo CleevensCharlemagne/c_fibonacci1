8a1bfc87adb37b3a200859a587fda7fbe03601e2
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-25 21:43:50 -0400

    update

--- main.c
+++ main.c
@@ -19,7 +19,11 @@ int fib(int n)
 	f[0] = 0;
 	f[1] = 1;
 
-
+	for (i = 2; i <= n; i++) {
+		/* Add the previous 2 numbers in the series
+		and store it */
+		f[i] = f[i - 1] + f[i - 2];
+	}
 
 	return f[n];
 }
